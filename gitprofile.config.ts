8d78db3971dcfa466270e695e8cb92b3a17cecef
Alejandro <josealejandrogomezparedes@gmail.com>
2024-08-31 17:49:46 -0500

    add: config changes

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -135,7 +135,7 @@ const CONFIG = {
     snippetVersion: 6,
   },
   themeConfig: {
-    defaultTheme: 'darcula',
+    defaultTheme: 'lofi',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
